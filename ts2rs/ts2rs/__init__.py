a67760b660be02ff072010e75c876bb5271d5989
Simon Berger <simon@siku2.io>
2020-08-17 22:04:06 +0200

    use single definition file

--- ts2rs/ts2rs/__init__.py
+++ ts2rs/ts2rs/__init__.py
@@ -2,11 +2,11 @@ from .js_enum import JsEnum
 from .js_namespace import JsNamespace
 from .js_object import JsObject
 
+#! FIXME handle constructor
+#! FIXME handle nested namespaces using js_namespace
+#! FIXME: check the docstring for "throws" and have them return Result<>
+
 # TODO use copy type when appropriate (keep track of which idents resolve to enums)
 # TODO Add "_" to Rust idents which conflict with keywords
-#! TODO handle constructor
-#! TODO handle nested namespaces using js_namespace
-
-
 # TODO maybe parse docstring to extract @param
-#! FIXME: check the docstring for "throws" and have them return Result<>
+# TODO automatically create string enum
